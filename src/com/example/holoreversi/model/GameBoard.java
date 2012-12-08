33a3985dbb05f8a34538807c78b245979b78f5e4
Eran Zimbler <eran@zimbler.net>
2012-12-08 17:41:28 +0200

    using 1 for white and 2 for black also moving engine code into the model no need for too much abstraction

--- src/com/example/holoreversi/model/GameBoard.java
+++ src/com/example/holoreversi/model/GameBoard.java
@@ -2,6 +2,9 @@ package com.example.holoreversi.model;
 
 import java.util.ArrayList;
 
+//import com.example.holoreversiEngine.move;
+//import com.example.holoreversiEngine.Board.TKind;
+
 public class GameBoard implements Board {
 
 	private int scoreWhite;
@@ -59,16 +62,24 @@ public class GameBoard implements Board {
 	}
 	@Override
 	public Cell[] getAllowedMovesWhite() {
-		Cell[] emptyCells = getEmpty();
-		return null;
+		return getAllowedCells(1);
 	}
 
 	@Override
 	public Cell[] getAllowedMovesBlack() {
-		Cell[] emptyCells = getEmpty();
-		return null;
+		return getAllowedCells(2);
 	}
-
+	private Cell[] getAllowedCells(int kind)
+	{
+		ArrayList<Cell> arr = new ArrayList<Cell>();
+		Cell[] emptyCells = getEmpty();
+		for (Cell cell : emptyCells) {
+			if(isValid(cell, 1))
+				arr.add(cell);
+		}
+		return (Cell[]) arr.toArray();
+	}
+	
 	@Override
 	public void addCallbackListener(Callback callback) {
 		// TODO Auto-generated method stub
@@ -89,7 +100,42 @@ public class GameBoard implements Board {
 	public int getScoreBlack() {
 		return scoreBlack;
 	}
-	
+	private int checkCell(int x,int y, int incx, int incy, int kind , boolean set)  {
+		// totally based with limited understanding on reversi.java.net
+		int opponent;
+		if (kind == 2) opponent=1; else opponent=2;
+		int n_inc=0;
+		x+=incx; y+=incy;
+		while ((x<boardSize) && (x>=0) && (y<boardSize) && (y>=0) && (tiles[x][y].contents==opponent)) {
+			x+=incx; y+=incy;
+			n_inc++;
+		}
+		if ((n_inc != 0) && (x<boardSize) && (x>=0) && (y<boardSize) && (y>=0) && (tiles[x][y].contents==kind)) {
+			 if (set)
+			 for (int j = 1 ; j <= n_inc ; j++) {
+				x-=incx; y-=incy;
+				 //set(new cell.x,cell.y(x,y),kind);
+			 }
+			return n_inc;
+		}
+		else return 0;
+	}
+	private boolean isValid(Cell cell, int kind) {
+		// check increasing x 
+		if (checkCell(cell.x,cell.y,1,0,kind,false) != 0) return true;
+		// check decreasing x 
+		if (checkCell(cell.x,cell.y,-1,0,kind,false) != 0) return true;
+		// check increasing y 
+		if (checkCell(cell.x,cell.y,0,1,kind,false) != 0) return true;
+		// check decreasing y 
+		if (checkCell(cell.x,cell.y,0,-1,kind,false) != 0) return true;
+		// check diagonals 
+		if (checkCell(cell.x,cell.y,1,1,kind,false) != 0) return true;
+		if (checkCell(cell.x,cell.y,-1,1,kind,false) != 0) return true;
+		if (checkCell(cell.x,cell.y,1,-1,kind,false) != 0) return true;
+		if (checkCell(cell.x,cell.y,-1,-1,kind,false) != 0) return true;
+		return false;
+	}
 	private boolean move(int x,int y, int player)
 	{
 		return true;
