a4381da49af1a6967683049033f513cd94472d96
Alex Gavrishev <alex.gavrishev@gmail.com>
2012-12-31 09:10:56 +0200

    add board icon

--- src/com/example/holoreversi/widget/ReplayFragment.java
+++ src/com/example/holoreversi/widget/ReplayFragment.java
@@ -1,7 +1,18 @@
 package com.example.holoreversi.widget;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.actionbarsherlock.app.SherlockFragment;
+import com.example.holoreversi.R;
 
 public class ReplayFragment extends SherlockFragment {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_replay, container, false);
+    }
 }
