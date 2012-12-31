460a5051d9fdd5ea20bd15cea5cfae6a22562515
Alex Gavrishev <alex.gavrishev@gmail.com>
2012-12-31 07:56:27 +0200

    add replay activity

---
+++ src/com/example/holoreversi/ReplayActivity.java
@@ -0,0 +1,23 @@
+package com.example.holoreversi;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.example.holoreversi.widget.HistoryListFragment;
+
+public class ReplayActivity extends SherlockFragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        // Create the list fragment and add it as our sole content.
+        if (fm.findFragmentById(android.R.id.content) == null) {
+        	HistoryListFragment list = new HistoryListFragment();
+            fm.beginTransaction().add(android.R.id.content, list).commit();
+        }
+	}
+}
---
+++ src/com/example/holoreversi/widget/ReplayFragment.java
@@ -0,0 +1,7 @@
+package com.example.holoreversi.widget;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class ReplayFragment extends SherlockFragment {
+
+}
